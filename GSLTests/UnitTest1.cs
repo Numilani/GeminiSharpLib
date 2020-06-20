4dc5e6f3eba1d12830d42cec7b5a400b91a5c746
John Arnold <john.e.arnold556@gmail.com>
2020-06-20 01:15:32 -0400

    Extracted code into multiple functions; prepping for delegates.

--- GSLTests/UnitTest1.cs
+++ GSLTests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using GeminiSharpLib;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
@@ -11,11 +14,27 @@ namespace GSLTests
         {
         }
 
+        public string fooResponse()
+        {
+            return "Welcome to FOOLAND, the funnest place on earth. Check out our other page, the CrowBAR.";
+        }
+        
+        public string barResponse()
+        {
+            return "Welcome to the CrowBAR; what can I get you to drink?";
+        }
+
         [Test]
         public void Test1()
         {
-            GeminiListener listener = new GeminiListener();
-            listener.run();
+            GeminiListener listener = new GeminiListener(new X509Certificate("C:\\x509\\geminiserver.pfx"), 1965);
+            Dictionary<String, GeminiRouteHandler> routes = new Dictionary<string, GeminiRouteHandler>();
+            routes.Add("foo", fooResponse);
+            routes.Add("bar", barResponse);
+            while (true)
+            {
+                listener.Listen(routes);   
+            }
         }
     }
 }
--- GeminiSharpLib/Class1.cs
+++ GeminiSharpLib/Class1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
@@ -8,49 +10,67 @@ using System.Text;
 
 namespace GeminiSharpLib
 {
+    public delegate string GeminiRouteHandler();
+    
     public class GeminiListener
     {
-        private const int portNum = 1965;
-        private const string CertFilename = "C:\\x509\\geminiserver.pfx";
+        // TODO: These should be moved to a config file.
+        // private const int portNum = 1965;
+        // private const string CertFilename = "C:\\x509\\geminiserver.pfx";
+
+        private X509Certificate _serverCert;
+        private TcpListener _listener;
         
-        public bool done = false;
-
-        public void run()
+        public GeminiListener(X509Certificate certificate, int portNum)
         {
-            var listener = new TcpListener(IPAddress.Any, portNum);
+            _serverCert = certificate;
+            _listener = new TcpListener(IPAddress.Any, portNum);
+        }
+
+        public void Listen(Dictionary<String, GeminiRouteHandler> routeHandlers)
+        {
+            _listener.Start();
             
-            listener.Start();
-            while (!done)
+            TcpClient client = _listener.AcceptTcpClient();
+
+            SslStream stream = new SslStream(client.GetStream());
+            stream.AuthenticateAsServer(_serverCert);
+
+            try
             {
-                Console.Write("Waiting for connection");
-                TcpClient client = listener.AcceptTcpClient();
-
-                Console.WriteLine("Connection accepted");
-                SslStream stream = new SslStream(client.GetStream());
+                byte[] request = new byte[1024];
+                stream.Read(request);
+                String uri = Encoding.UTF8.GetString(request).TrimEnd('\u0000');
                 
-                stream.AuthenticateAsServer(new X509Certificate(CertFilename));
+                List<String> path = new List<string>(uri.Split("/"));
+                path.RemoveRange(0,2);
+                
+                // TestResponse(stream);
+                // if (routeHandlers.ContainsKey(path[1]))
+                // {
+                    // Success(stream, routeHandlers[path[1]]);
+                    Success(stream);
+                // }
 
-                byte[] byteHeader = Encoding.UTF8.GetBytes("20 SUCCESS\r\n");
-                byte[] byteBody = Encoding.UTF8.GetBytes("This is a test.\r\n");
-
-                try
-                {
-                    byte[] request = new byte[1024];
-                    stream.Read(request);
-                    Console.WriteLine("Request is: " + Encoding.UTF8.GetString(request).TrimEnd('\u0000'));
-                    stream.Write(byteHeader, 0, byteHeader.Length);
-                    stream.Write(byteBody, 0, byteBody.Length);
-                    stream.Close();
-                    client.Close();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-
-                done = true;
+                stream.Close();
+                client.Close();
             }
-            listener.Stop();
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            _listener.Stop();
+        }
+
+        // private static void Success(SslStream stream, GeminiRouteHandler body)
+        private static void Success(SslStream stream)
+        {
+            byte[] byteHeader = Encoding.UTF8.GetBytes("20 SUCCESS\r\n");
+            byte[] byteBody = Encoding.UTF8.GetBytes("This is a test\r\n");
+
+            stream.Write(byteHeader, 0, byteHeader.Length);
+            stream.Write(byteBody, 0, byteBody.Length);
         }
     }
 }
